f1229f91f30943aeff0cb3a9d0781935da6c4176
dknlinh <wackyroshi@protonmail.com>
2026-07-02 13:37:03 +0700

    update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,7 +10,7 @@ require "kickstart.plugins.telescope"
 require "kickstart.plugins.lspconfig"
 require "kickstart.plugins.conform"
 require "kickstart.plugins.blink-cmp"
-require "kickstart.plugins.treesitter"
+-- require "kickstart.plugins.treesitter"
 
 -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 -- init.lua. If you want these files, they are in the repository, so you can just download them and
